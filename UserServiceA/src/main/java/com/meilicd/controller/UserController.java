e580afd39cfe994277902de937d239490fdf737c
QL\hanyingqiang <hanyingqiang@mobanker.com>
2018-08-27 18:31:56 +0800

    temp

--- UserServiceA/src/main/java/com/meilicd/controller/UserController.java
+++ UserServiceA/src/main/java/com/meilicd/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.meilicd.controller;
 
 import com.meilicd.dto.UserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @RequestMapping(value = "/getUser/{userId}",method = RequestMethod.GET)
     public UserDto getUser(@PathVariable("userId") Long userId){
         UserDto userDto = new UserDto();
